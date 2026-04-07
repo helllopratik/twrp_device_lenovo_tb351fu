7a5ba1a6a8cb38d1a8c725df0f58e6e46f3ce4c4
helllopratik <pratikgondane07@gmail.com>
2026-04-07 17:53:28 +0530

    fix AVB issue

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Clean up the problematic vendor symlink/directory before build
+# Automatically clean up the problematic root/vendor directory before each build
+# This fixes the rsync error 'could not make way for new symlink'
 rm -rf out/target/product/TB351FU/root/vendor
 rm -rf out/target/product/TB351FU/recovery/root/vendor
