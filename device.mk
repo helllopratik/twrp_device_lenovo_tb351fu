ae12511d9392798bc759113ef38565d56b62867d
helllopratik <pratikgondane07@gmail.com>
2026-04-07 10:21:03 +0530

    fix recovery VINTF confits

--- device.mk
+++ device.mk
@@ -21,8 +21,11 @@ AB_OTA_PARTITIONS := \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/dtb.img:dtb.img
 
-# Include stock vendor ramdisk files
-PRODUCT_COPY_FILES += $(foreach f,$(shell find $(LOCAL_PATH)/prebuilt/vendor_ramdisk -type f),$(f):$(subst $(LOCAL_PATH)/prebuilt/vendor_ramdisk/,,$(f)))
+# Include stock vendor ramdisk files (excluding VINTF to avoid build errors)
+PRODUCT_COPY_FILES += $(foreach f,$(shell find $(LOCAL_PATH)/prebuilt/vendor_ramdisk -type f -not -path "*/vintf/*"),$(f):$(subst $(LOCAL_PATH)/prebuilt/vendor_ramdisk/,,$(f)))
+
+# Add VINTF fragments properly
+DEVICE_MANIFEST_FILE += $(wildcard $(LOCAL_PATH)/prebuilt/vintf/*.xml)
 
 # Enable EROFS support (Required for ZUI 17)
 PRODUCT_FS_COMPRESSION := true
